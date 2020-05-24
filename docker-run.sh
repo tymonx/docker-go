91a2da84abbf0c87cd1437bc9d2156c35a390491
Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com>
2020-05-24 10:51:27 +0200

    Added exit on error

--- docker-run.sh
+++ docker-run.sh
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Exit on error
+set -eo pipefail
+
 # First capture all command line arguments to a separate variable.
 # This will allow to preserve command invocation from unwanted behavior
 DOCKER_ARGUMENTS=$@
